cec2bc6aeeaba7ee2a05c1d50d1b84b672015818
Mehrdad Oliaee <mehr1dado@gmail.com>
2023-06-09 07:47:40 +0330

    ADD!

---
+++ node/dev/providers/clashnode/ua.py
@@ -0,0 +1,17 @@
+import urllib.request
+import re
+
+url = "https://raw.githubusercontent.com/mlabalabala/v2ray-node/main/clashnode4clash.txt"
+output_file = "./node/clashnode/ua.yml"
+
+def export_us_content():
+    with urllib.request.urlopen(url) as f:
+        content = f.read().decode('utf-8')
+    filtered_content = re.findall(r'^.*name: 🇺🇦.*', content, flags=re.MULTILINE)
+    with open(output_file, "w") as f:
+        f.write("proxies:\n")  # Add proxies: at the beginning of the file
+        for line in filtered_content:
+            f.write(line + "\n")
+    print("🇺🇦UA exported.")
+
+export_us_content()

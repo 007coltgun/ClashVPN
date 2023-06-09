24d3225ea0c148d51f110234068554c472b8370a
Mehrdad Oliaee <44939513+MEHR1DAD@users.noreply.github.com>
2023-06-09 13:34:13 +0330

    Update run_all.py

--- node/dev/providers/nodefree/run_all.py
+++ node/dev/providers/nodefree/run_all.py
@@ -7,6 +7,7 @@ def export_content(output_file, pattern):
     with urllib.request.urlopen(url) as f:
         content = f.read().decode('utf-8')
     filtered_content = re.findall(pattern, content, flags=re.MULTILINE)
+    filtered_content = [line for line in filtered_content if "UUID" in line]  # Add a filter to include only lines that contain UUID
     with open(output_file, "w") as f:
         f.write("proxies:\n")  # Add proxies: at the beginning of the file
         for line in filtered_content:
